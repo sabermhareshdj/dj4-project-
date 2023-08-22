15fa2f9dd61ab5b532c8c78ad44cba5d9ff9b85d
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-08-22 18:14:33 +0300

    add admin customization for product and admin-tabular

--- product/admin.py
+++ product/admin.py
@@ -4,7 +4,17 @@ from django.contrib import admin
 from .models import Product , ProductImages , Brand , Review
 
 
-admin.site.register(Product)
+class ProductImagesTabular(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'flag' , 'price' , 'quantity' , 'brand']
+    list_filter = ['flag' , 'brand']
+    search_fields = ['name','subtitle','description']
+    inlines = [ProductImagesTabular]
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImages)
 admin.site.register(Brand)
 admin.site.register(Review)
